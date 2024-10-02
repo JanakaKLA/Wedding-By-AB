63f627649db6d3ce01de641ef6212b4a922d4a71
JanakaKLA <155962195+JanakaKLA@users.noreply.github.com>
2024-10-02 15:54:09 +0530

    facebook and insta buttons add

--- frontend/src/components/ItemDisplay/ItemDisplay.jsx
+++ frontend/src/components/ItemDisplay/ItemDisplay.jsx
@@ -1,24 +1,95 @@
-import React, { useContext } from 'react'
-import './ItemDisplay.css'
-import { StoreContext } from '../../Context/StoreContext'
-import PlaceItem from '../PlaceItem/PlaceItem'
+// import React, { useContext } from 'react'
+// import './ItemDisplay.css'
+// import { StoreContext } from '../../Context/StoreContext'
+// import PlaceItem from '../PlaceItem/PlaceItem'
 
-const ItemDisplay = ({category}) => {
+// const ItemDisplay = ({category}) => {
 
-    const {place_list} = useContext(StoreContext)
-  return (
-    <div className='Item-display' id='item-display'>
-        <h2>Top choises.</h2>
-        <div className="item-display-list">
-            {place_list.map((num,index)=>{
-              if (category==="All" || category===num.category){
-                return <PlaceItem key={index} id={num._id} name={num.name} description={num.description} price={num.price} image={num.image}/>
-              }
+//     const {place_list} = useContext(StoreContext)
+//   return (
+//     <div className='Item-display' id='item-display'>
+//         <h2>Top choises.</h2>
+//         <div className="item-display-list">
+//             {place_list.map((num,index)=>{
+//               if (category==="All" || category===num.category){
+//                 return <PlaceItem key={index} id={num._id} name={num.name} description={num.description} price={num.price} image={num.image}/>
+//               }
                
-            })}
+//             })}
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default ItemDisplay
+
+// import React, { useContext } from 'react';
+// import './ItemDisplay.css';
+// import { StoreContext } from '../../Context/StoreContext';
+// import PlaceItem from '../PlaceItem/PlaceItem';
+
+// const ItemDisplay = ({ category }) => {
+//     const { place_list } = useContext(StoreContext);
+
+//     return (
+//         <div className='Item-display' id='item-display'>
+//             <h2>Top choices.</h2>
+//             <div className="item-display-list">
+//                 {place_list.map((num, index) => {
+//                     if (category === "All" || category === num.category) {
+//                         return (
+//                             <PlaceItem 
+//                                 key={index} 
+//                                 id={num._id} 
+//                                 name={num.name} 
+//                                 description={num.description} 
+//                                 price={num.price} 
+//                                 image={num.image} 
+//                                 fbLink={num.fbLink} // Pass fbLink as a prop
+//                             />
+//                         );
+//                     }
+//                     return null; // Ensure to return null if the condition is not met
+//                 })}
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default ItemDisplay;
+
+
+import React, { useContext } from 'react';
+import './ItemDisplay.css';
+import { StoreContext } from '../../Context/StoreContext';
+import PlaceItem from '../PlaceItem/PlaceItem';
+
+const ItemDisplay = ({ category }) => {
+    const { place_list } = useContext(StoreContext);
+
+    return (
+        <div className='Item-display' id='item-display'>
+            <h2>Top choices.</h2>
+            <div className="item-display-list">
+                {place_list.map((num, index) => {
+                    if (category === "All" || category === num.category) {
+                        return (
+                            <PlaceItem 
+                                key={index} 
+                                id={num._id} 
+                                name={num.name} 
+                                description={num.description}  
+                                image={num.image} 
+                                fbLink={num.fbLink} // Pass fbLink as a prop
+                                instagramLink={num.instagramLink} // Pass instagramLink as a prop
+                            />
+                        );
+                    }
+                    return null; // Ensure to return null if the condition is not met
+                })}
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default ItemDisplay
+export default ItemDisplay;
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../Context/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartItems, place_list, removeFromCart } = useContext(StoreContext);
+  const navigate = useNavigate();
 
   return (
     <div>
@@ -30,6 +32,9 @@ const Cart = () => {
               );
             }
           })}
+        </div>
+        <div className="cart-bottom">
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
       </div>
     </div>
